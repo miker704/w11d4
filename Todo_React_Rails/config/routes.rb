9780dc9e1f938543fb442029da054cb17fd86f07
miker704 <miker704@gmail.com>
2022-06-19 16:58:48 -0400

    cw

--- Todo_React_Rails/config/routes.rb
+++ Todo_React_Rails/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+
+  namespace :api, defaults: { format: :json } do
+    resources :todos, only: [:index, :show, :create, :update, :destroy] do
+      resources :steps, only: [:index, :create]
+    end
+
+    resources :steps, only: [:update, :destroy]
+  end
 end
