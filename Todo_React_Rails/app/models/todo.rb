2d38d33da8b130f79f9e05d202f5f25420918494
miker704 <miker704@gmail.com>
2022-06-19 16:40:38 -0400

    cw

--- Todo_React_Rails/app/models/todo.rb
+++ Todo_React_Rails/app/models/todo.rb
@@ -19,9 +19,9 @@ class Todo < ApplicationRecord
     validates :title, :body, presence: true
     validates :done, inclusion: {in: [true,false]}
 
-    belongs_to :user, class_name: "user", foreign_key: "user_id"
+    belongs_to :user
     
-    has_many :steps, class_name: "steps", foreign_key: "step_id"
+    has_many :steps
     has_many :taggings
     has_many :tags, through: :taggings, source: :tag
 
