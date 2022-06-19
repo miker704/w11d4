d9e66902788ce2dc6f7b4a3899a5800be53ce762
miker704 <miker704@gmail.com>
2022-06-19 16:18:27 -0400

    cw

--- Todo_React_Rails/app/models/todo.rb
+++ Todo_React_Rails/app/models/todo.rb
@@ -9,7 +9,7 @@ class Todo < ApplicationRecord
     has_many :taggings
     has_many :tags, through: :taggings, source: :tag
 
-    def tag_names =(tag_names)
+    def tag_names=(tag_names)
         self.tags = tag_names.map do |tag_name|
             Tag.find_or_create_by(name:tag_name)
         end
